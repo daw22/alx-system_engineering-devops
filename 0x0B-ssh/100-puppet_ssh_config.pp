2c3ae9c4633d41ea530702b5c83c50528e8b027c
Dawit Yifru <dawitxxd@gmail.com>
2024-04-20 17:00:35 +0300

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# update client ssh config to use private key Auth and not password
+
+file_line {'Turn of password Auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	passwordAuthentication no',
+  replace => true,
+}
+
+file_line {'Set prv key to use for Auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentityFile ~/.ssh/school',
+  replace => true,
+}
+

bc60b828ffa542168887c0445a8b04c662cfbc01
Dawit Yifru <dawitxxd@gmail.com>
2024-04-20 17:57:11 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,7 +11,7 @@ file_line {'Turn of password Auth':
 file_line {'Set prv key to use for Auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '	IdentityFile ~/.ssh/school',
+  line    => '	IdentitfyFile ~/.ssh/school',
   replace => true,
 }
 

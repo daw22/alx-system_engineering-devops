7bf50f89cddac4dcac171cc4a9a7e9c3cc48b519
Dawit Yifru <dawitxxd@gmail.com>
2024-05-20 21:49:53 +0300

    Export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_id = sys.argv[1]
+    full_url = base_url + "/" + user_id
+
+    response = requests.get(full_url).json()
+    user_name = response.get("username")
+
+    todos_url = full_url + "/" + "todos"
+    todos = requests.get(todos_url).json()
+
+    dictionary = {user_id: []}
+    for todo in todos:
+        dictionary[user_id].append({
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user_name
+            })
+    with open("{}.json".format(user_id), "w") as f:
+        for todo in todos:
+            json.dump(dictionary, f)

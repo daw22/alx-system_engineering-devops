2d9345be7bc522970ad72fcfe55756b0ba7e4909
Dawit Yifru <dawitxxd@gmail.com>
2024-06-05 00:39:46 +0300

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,18 @@ A script to get number of subscribers of a subreddit
 """
 
 import requests
-
+import sys
 
 def number_of_subscribers(subreddit):
     """
-    Quersie Reddit api and returns the number of
+    Queries Reddit api and returns the number of
     subscribers for a given subredit
     Args:
         subreddit: name of the subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     try:
-        response = requests.get(url)
+        response = requests.get(url, allow_redirects=False)
         response.raise_for_status()
         data = response.json()
         return data.get("data", {}).get("subscribers", 0)

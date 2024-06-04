c8b067015ee1dc8ea38810f86d48dbdb0e17c12b
Dawit Yifru <dawitxxd@gmail.com>
2024-06-04 21:52:48 +0300

    top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A script that prints top ten posts of a
+subreddit
+"""
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """
+    prints top ten posts of a subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    try:
+        headers = {'User-Agent': """My Reddit Client /v1.0
+                   (by /u/Professional-Two9987)"""}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        if len(posts) > 0:
+            for post in posts:
+                print(post.get("data").get("title"))
+        else:
+            print("None")
+    except Exception as e:
+        print(e)
+        print("None")

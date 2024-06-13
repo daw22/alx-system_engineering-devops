e8169c078f2b0bb6e0879e11fd2ace40fba2feb5
Dawit Yifru <dawitxxd@gmail.com>
2024-06-13 22:30:10 +0300

    server overload

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# fix server overload problem
+
+exec {'relace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
